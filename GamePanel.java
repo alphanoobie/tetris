04350ad52b2ea1e818483aabf75acd1ee7c64c60
alphanoobie <hussaingadi.hg@gmail.com>
2024-06-20 22:10:52 +0930

    format

--- GamePanel.java
+++ GamePanel.java
@@ -8,7 +8,7 @@ public class GamePanel extends JPanel {
     public static final int WIDTH = 1280;
     public static final int HEIGHT = 720;
 
-    public GamePanel(){
+    public GamePanel() {
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setBackground(Color.black);
         this.setLayout(null);
