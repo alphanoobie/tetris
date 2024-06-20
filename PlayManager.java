182f627d02074fe6b167a196e3c78068cc69ab7a
alphanoobie <hussaingadi.hg@gmail.com>
2024-06-20 22:39:28 +0930

    next area

--- PlayManager.java
+++ PlayManager.java
@@ -1,7 +1,9 @@
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 public class PlayManager {
 
@@ -28,6 +30,14 @@ public class PlayManager {
         g2.setColor(Color.white);
         g2.setStroke(new BasicStroke(4f));
         g2.drawRect(left_x - 4, top_y - 4, WIDTH + 8, HEIGHT + 8);
+
+        int x = right_x + 100;
+        int y = bottom_y - 200;
+        g2.drawRect(x, y, 200, 200);
+        g2.setFont(new Font("Arial", Font.PLAIN, 30));
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2.drawString("NEXT", x + 60, y + 60);
+
     }
 
 }
